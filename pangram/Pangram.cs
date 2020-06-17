419493f13091f64295fd770f574189a9215265d2
Coburn,Jeff <jeff.coburn@colostate.edu>
2020-06-17 16:02:08 -0600

    Hello-world, TowFer, Panagram

---
+++ pangram/Pangram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        for (char letter='A'; letter <='Z'; letter++)
+        {
+            if (!input.Contains(letter,StringComparison.OrdinalIgnoreCase)) { return false; }
+        }
+        return true;
+    }
+}
