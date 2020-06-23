a7d952d220e379a5ef3d50a816db929153bee43b
Jeff Coburn <jeff.coburn@colostate.edu>
2020-06-23 15:02:20 -0600

    Resistor color working using ternary operator

--- resistor-color/ResistorColor.cs
+++ resistor-color/ResistorColor.cs
@@ -3,21 +3,18 @@
 public static class ResistorColor
 {
     public static int ColorCode(string color) 
-    {
-        switch (color.ToLower())
-        {
-            case "black": return 0;
-            case "brown": return 1;
-            case "red": return 2;
-            case "orange": return 3;
-            case "yellow": return 4;
-            case "green": return 5;
-            case "blue": return 6;
-            case "violet": return 7;
-            case "grey": return 8;
-            case "white": return 9;
-            default: throw new Exception("Invalid color");
-        };
+    {          
+        color = color.ToLower();
+        return (color=="black"? 0 
+            : color=="brown" ? 1   
+            : color=="red" ? 2 
+            : color=="orange" ? 3 
+            : color=="yellow" ? 4 
+            : color=="green" ? 5
+            : color=="blue" ? 6
+            : color=="violet" ? 7
+            : color=="grey" ? 8
+            : color=="white" ? 9 : throw new Exception("Invalid color"));
     }
 
     public static string[] Colors() => (new string[] {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"});
