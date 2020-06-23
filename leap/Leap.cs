eb147e3246e0eca4f7c1a0425c7d3dfae9fd0bc0
Jeff Coburn <jeff.coburn@colostate.edu>
2020-06-23 11:17:21 -0600

    Removed if statements

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -2,17 +2,9 @@ using System;
 
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
+    public static bool IsLeapYear(int year) 
     {
-        if (year%4==0) 
-        {
-            if (year%100==0)
-            {
-                if (year%400==0) return true;
-                return false;
-            }
-            return true;
-        }
-        return false;
+        return year%4==0 && year%100!=0 || year%400==0;
     }
 }
+
