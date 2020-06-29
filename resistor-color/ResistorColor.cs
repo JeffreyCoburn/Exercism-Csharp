318a02e19f3b6edc3f9fb993968168e1aecb139a
Jeff Coburn <jeff.coburn@colostate.edu>
2020-06-29 08:59:25 -0600

    Changed to using Array.IndexOf

--- resistor-color/ResistorColor.cs
+++ resistor-color/ResistorColor.cs
@@ -5,16 +5,7 @@ public static class ResistorColor
     public static int ColorCode(string color) 
     {          
         color = color.ToLower();
-        return (color=="black"? 0 
-            : color=="brown" ? 1   
-            : color=="red" ? 2 
-            : color=="orange" ? 3 
-            : color=="yellow" ? 4 
-            : color=="green" ? 5
-            : color=="blue" ? 6
-            : color=="violet" ? 7
-            : color=="grey" ? 8
-            : color=="white" ? 9 : throw new Exception("Invalid color"));
+        return Array.IndexOf(Colors(),color);
     }
 
     public static string[] Colors() => (new string[] {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"});
