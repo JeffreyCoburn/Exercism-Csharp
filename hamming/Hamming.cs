73a343bdf8e8270cff4145f073d482dc7cfd46aa
Coburn, Jeff <jeff.coburn@colosate.edu>
2020-07-12 10:02:24 -0600

    Added whitespace for readability

--- hamming/Hamming.cs
+++ hamming/Hamming.cs
@@ -4,10 +4,13 @@ public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        int count = 0;
+        int distance = 0;
         if (firstStrand.Length != secondStrand.Length)
             throw new ArgumentException("Lengths of strands are not equal.");        
-        for (int i = 0; i < firstStrand.Length; i++) if (!firstStrand[i].Equals(secondStrand[i])) count++;
-        return count;
+        for (int i = 0; i < firstStrand.Length; i++) 
+        {
+            if (!firstStrand[i].Equals(secondStrand[i])) distance++;
+        }
+        return distance;
     }
 }
