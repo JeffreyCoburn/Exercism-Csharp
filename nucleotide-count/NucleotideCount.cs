0401ac0e11f837ef81b6fa7e9f51c5928efff3f8
Jeff Coburn <jeff.coburn@colostate.edu>
2020-07-14 10:59:32 -0600

    Changed dictionary initialization

--- nucleotide-count/NucleotideCount.cs
+++ nucleotide-count/NucleotideCount.cs
@@ -5,21 +5,22 @@ public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        IDictionary<char, int> _Count =
-            new Dictionary<char, int>();
-
-        _Count.Add('A', 0);
-        _Count.Add('C', 0);
-        _Count.Add('G', 0);
-        _Count.Add('T', 0);
+        IDictionary<char, int> count =
+            new Dictionary<char, int>()
+            {
+                { 'A', 0 },
+                { 'C', 0 },
+                { 'G', 0 },
+                { 'T', 0 }
+            };
 
         for (int i=0; i < sequence.Length; i++)
         {
-            if (_Count.ContainsKey(sequence[i]))
-                _Count[sequence[i]]++;
+            if (count.ContainsKey(sequence[i]))
+                count[sequence[i]]++;
             else
                 throw new ArgumentException("Invalid character in sequence."); 
         }
-        return _Count;
+        return count;
     }
 }
