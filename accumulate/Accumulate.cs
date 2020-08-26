8a99557cf55c2674cf71d8cedd6dc8ce95d5043c
Jeff Coburn <jeff.coburn@colostate.edu>
2020-08-25 19:20:38 -0600

    Changed to use yield return, now passing all tests

--- accumulate/Accumulate.cs
+++ accumulate/Accumulate.cs
@@ -5,11 +5,10 @@ public static class AccumulateExtensions
 {
     public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
     {
-        ICollection<U> results = new List<U>();
         foreach (T item in collection)
         {
-            results.Add(func(item));
+            yield return func(item);
         }
-        return results;
+        
     }
 }
