d2af16825a293cb2a1a829acdfea8cc70fc6aee9
JeffreyCoburn <65735306+JeffreyCoburn@users.noreply.github.com>
2020-09-08 12:40:57 -0600

    Side exercise difference of squares (#22)
    * Difference of Squares passing all tests
    
    * Added code to run in parallel in comment block
    
    Co-authored-by: Coburn, Jeff <jeff.coburn@colosate.edu>

---
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max) => (int) Math.Pow(Enumerable.Range(1,max).Sum(),2);
+
+    public static int CalculateSumOfSquares(int max)
+    {
+        /* Parallel was slower
+        int result = 0;
+        Parallel.ForEach(Enumerable.Range(1,max).ToArray(), 
+                                        x => result += (int) Math.Pow(x,2));
+        return result; */
+        return (int) Enumerable.Range(1,max).Select(x => Math.Pow(x,2)).Sum();
+    }
+
+    public static int CalculateDifferenceOfSquares(int max) => CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+}
