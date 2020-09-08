c773ffcee69d0964f100f1d09cf559b71c32aed6
Coburn, Jeff <jeff.coburn@colosate.edu>
2020-09-08 12:10:31 -0600

    Difference of Squares passing all tests

---
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max) => (int) Math.Pow(Enumerable.Range(1,max).Sum(),2);
+
+    public static int CalculateSumOfSquares(int max)
+    {
+        //var test = Enumerable.Range(1,max).AsParallel().ForAll( x => Math.Pow(x,2));
+        return (int) Enumerable.Range(1,max).Select(x => Math.Pow(x,2)).Sum();
+    }
+
+    public static int CalculateDifferenceOfSquares(int max) => CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+}
