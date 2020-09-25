62e585e9120c25fb12c7ebad4774014aaf7bb0fe
Coburn, Jeff <jeff.coburn@colosate.edu>
2020-09-25 15:09:51 -0600

    Fixed initialization of b (to ensure it's > a)

--- pythagorean-triplet/PythagoreanTriplet.cs
+++ pythagorean-triplet/PythagoreanTriplet.cs
@@ -8,7 +8,7 @@ public static class PythagoreanTriplet
         IList<(int, int, int)> triplets = new List<(int, int, int)>();
         int c;
         for (int a=1; a < sum/3; a++) {
-            for (int b=a; b < sum/2; b++) {
+            for (int b=a+1; b < sum/2; b++) {
                 c = sum-a-b;        
                 if (a*a + b*b == c*c) {
                     triplets.Add((a,b,c));
