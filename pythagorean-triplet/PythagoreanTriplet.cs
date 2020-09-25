f3a0e53f5698b502c80cc9e2b3b4e94d4c79e427
Coburn, Jeff <jeff.coburn@colosate.edu>
2020-09-25 15:03:00 -0600

    Removed unnecessary variable initialization

--- pythagorean-triplet/PythagoreanTriplet.cs
+++ pythagorean-triplet/PythagoreanTriplet.cs
@@ -6,11 +6,9 @@ public static class PythagoreanTriplet
     public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
     {
         IList<(int, int, int)> triplets = new List<(int, int, int)>();
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        for (a = 1; a < sum / 3; a++) {
-            for (b = a; b < sum / 2; b++) {
+        int c;
+        for (int a = 1; a < sum / 3; a++) {
+            for (int b = a; b < sum / 2; b++) {
                 c = sum - a - b;        
                 if (a * a + b * b == c * c) {
                     triplets.Add((a,b,c));
