2698dab87e6ada3bed1a845f503f0816097ccfbe
Coburn, Jeff <jeff.coburn@colosate.edu>
2020-09-28 11:03:49 -0600

    Fixed math

--- pythagorean-triplet/PythagoreanTriplet.cs
+++ pythagorean-triplet/PythagoreanTriplet.cs
@@ -7,8 +7,9 @@ public static class PythagoreanTriplet
     {
         IList<(int, int, int)> triplets = new List<(int, int, int)>();
         int c;
-        for (int a=1; a < sum/3; a++) {
-            for (int b=a+1; b < sum/2; b++) {
+        // Since a<b<c and a+b+c=sum, a+(a+1)+(a+2)=3a+3=sum
+        for (int a=1; a <= (sum-3)/3; a++) {
+            for (int b=a+1; b < (sum-1)/2; b++) {
                 c = sum-a-b;        
                 if (a*a + b*b == c*c) {
                     triplets.Add((a,b,c));
