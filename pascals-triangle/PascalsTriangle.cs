f05e5b501c259902ad43e06661da17594bda40ec
JeffreyCoburn <65735306+JeffreyCoburn@users.noreply.github.com>
2020-10-28 21:06:14 -0600

    Pascals triangle (#42)
    * Changed logic to set the first and last columns equal to 1 so I don't have to check the column value before setting the left and right values
    
    * Got rid of extraneous commented code, and unnecessary initialization
    
    Co-authored-by: Coburn, Jeff <jeff.coburn@colosate.edu>

--- pascals-triangle/PascalsTriangle.cs
+++ pascals-triangle/PascalsTriangle.cs
@@ -5,13 +5,14 @@ public static class PascalsTriangle
 {
     public static IEnumerable<IEnumerable<int>> Calculate(int rows)
     {
-        List<int[]> pascalsTriangle = new List<int[]>(); // { {1} };
-        if (rows > 0) { pascalsTriangle.Add(new int[] {1}); }
-        for (int row = 1; row < rows; row++) {
-            int[] rowValues = new int[row+1];            
-            for (int column = 0; column <= row; column++) {
-                int left = column > 0 ? pascalsTriangle[row-1][column-1] : 0;
-                int right = column < row ? pascalsTriangle[row-1][column] : 0;
+        List<int[]> pascalsTriangle = new List<int[]>(); 
+        for (int row = 0; row < rows; row++) {
+            int[] rowValues = new int[row+1];
+            rowValues[0] = 1;
+            rowValues[row] = 1;        
+            for (int column = 1; column < row; column++) {
+                int left = pascalsTriangle[row-1][column-1];
+                int right = pascalsTriangle[row-1][column];
                 rowValues[column] = left + right;
             }
             pascalsTriangle.Add(rowValues);
