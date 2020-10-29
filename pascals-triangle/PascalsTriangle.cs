8f27d222eb297a901e1c5e05104e4ef88e260479
Coburn, Jeff <jeff.coburn@colosate.edu>
2020-10-28 20:49:42 -0600

    Passing all tests

---
+++ pascals-triangle/PascalsTriangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class PascalsTriangle
+{
+    public static IEnumerable<IEnumerable<int>> Calculate(int rows)
+    {
+        List<int[]> pascalsTriangle = new List<int[]>(); // { {1} };
+        if (rows > 0) { pascalsTriangle.Add(new int[] {1}); }
+        for (int row = 1; row < rows; row++) {
+            int[] rowValues = new int[row+1];            
+            for (int column = 0; column <= row; column++) {
+                int left = column > 0 ? pascalsTriangle[row-1][column-1] : 0;
+                int right = column < row ? pascalsTriangle[row-1][column] : 0;
+                rowValues[column] = left + right;
+            }
+            pascalsTriangle.Add(rowValues);
+        }
+        return pascalsTriangle;
+    }
+
+}
