cdca8dd5a55370f99907c5e29775de6b0ba4c119
Jeff Coburn <jeff.coburn@colostate.edu>
2020-11-11 14:32:14 -0700

    Passing all tests, waiting on approval

--- matching-brackets/MatchingBrackets.cs
+++ matching-brackets/MatchingBrackets.cs
@@ -1,41 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 public static class MatchingBrackets
 {
+    private static Dictionary<char, char> brackets = new Dictionary<char, char> {
+        {']', '['},
+        {'}', '{'},
+        {')', '('}
+    };
+
     public static bool IsPaired(string input)
     {
         // If string is empty, return true
         if (input.Length==0) { return true; }
 
-        int endBracketIndex;
-        // Check if the first character is a bracket
-        switch (input[0]) {
-            case '[':
-                endBracketIndex = input.LastIndexOf(']');
-                // If the matching end bracket can't be found, return false
-                if (endBracketIndex == -1) { return false; };
-                break;
-            case '{':
-                endBracketIndex = input.LastIndexOf('}');
-                // If the matching end bracket can't be found, return false
-                if (endBracketIndex == -1) { return false; };
-                break;
-            case '(':
-                endBracketIndex = input.LastIndexOf(')');
-                // If the matching end bracket can't be found, return false
-                if (endBracketIndex == -1) { return false; };
-                break;
-            default:
-                // Otherwise, strip off first char since it isn't a bracket, and check the next
-                return IsPaired(input.Substring(1,input.Length-1));
-           
+        Stack<char> startingBrackets = new Stack<char>();
+
+        for (int i=0; i < input.Length; i++) {
+            // If character is a starting bracket, add it to the stack
+            if ( brackets.ContainsValue(input[i])) { startingBrackets.Push(input[i]); continue; };
+            // If character is an ending bracket, pop the last bracket off the stack and verify it matches
+            if ( brackets.ContainsKey(input[i])) {
+                // If the stack is empty, there is an ending bracket with no starting bracket
+                if (startingBrackets.Count == 0) { return false; }
+                // Check if the ending bracket matches the last starting bracket
+                char lastBracket = startingBrackets.Pop();
+                if (lastBracket != brackets[input[i]]) { return false; };
+            }
         }
-        // If the brackets match, check the string inside the bracket as well as the string after the end bracket (if there is any)
-        if (endBracketIndex == input.Length - 1) {
-            return IsPaired(input.Substring(1, endBracketIndex - 1));
-        }
-        return IsPaired(input.Substring(1, endBracketIndex - 1)) &&
-                IsPaired(input.Substring(endBracketIndex + 1, input.Length - endBracketIndex - 1));
+
+        // If stack is empty, that means every starting bracket had an ending bracket
+        if (startingBrackets.Count == 0) { return true; }
+        return false;
+
+        
     }
 
 }
