3a2a8adcaf314de79484b989f6547ef70f8c7c76
Jeff Coburn <jeff.coburn@colostate.edu>
2020-11-11 14:42:51 -0700

    Removed unnecessary if

--- matching-brackets/MatchingBrackets.cs
+++ matching-brackets/MatchingBrackets.cs
@@ -10,22 +10,18 @@ public static class MatchingBrackets
     };
 
     public static bool IsPaired(string input)
-    {
-        // If string is empty, return true
-        if (input.Length==0) { return true; }
-
+    {        
         Stack<char> startingBrackets = new Stack<char>();
 
         for (int i=0; i < input.Length; i++) {
             // If character is a starting bracket, add it to the stack
             if ( brackets.ContainsValue(input[i])) { startingBrackets.Push(input[i]); continue; };
-            // If character is an ending bracket, pop the last bracket off the stack and verify it matches
+            // If character is an ending bracket...
             if ( brackets.ContainsKey(input[i])) {
                 // If the stack is empty, there is an ending bracket with no starting bracket
                 if (startingBrackets.Count == 0) { return false; }
                 // Check if the ending bracket matches the last starting bracket
-                char lastBracket = startingBrackets.Pop();
-                if (lastBracket != brackets[input[i]]) { return false; };
+                if (startingBrackets.Pop() != brackets[input[i]]) { return false; };
             }
         }
 
