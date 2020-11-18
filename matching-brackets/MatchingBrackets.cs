1961209efdd781ef5161e8fa89a6aa106b3a01ad
Jeff Coburn <jeff.coburn@colostate.edu>
2020-11-18 09:29:04 -0700

    My preferred solution

--- matching-brackets/MatchingBrackets.cs
+++ matching-brackets/MatchingBrackets.cs
@@ -14,9 +14,12 @@ public static class MatchingBrackets
         Stack<char> startingBrackets = new Stack<char>();
 
         for (int i=0; i < input.Length; i++) {
-            if ( brackets.ContainsValue(input[i])) { startingBrackets.Push(input[i]); };
-            if ( brackets.ContainsKey(input[i]) && ((startingBrackets.Count == 0) || (startingBrackets.Pop() != brackets[input[i]]))) {
-                return false; 
+            if ( brackets.ContainsValue(input[i])) { startingBrackets.Push(input[i]); continue; };
+            if ( brackets.ContainsKey(input[i])) {
+                // If the stack is empty, there is an ending bracket with no starting bracket
+                if (startingBrackets.Count == 0) { return false; }
+                // Check if the ending bracket matches the last starting bracket
+                if (startingBrackets.Pop() != brackets[input[i]]) { return false; };
             }
         }
 
